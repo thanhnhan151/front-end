73ad1daa5f224e5c3806ee8903ac84e29f3d1236
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-11-29 13:04:54 +0700

    Add Tailwind and bunch of new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
 import { ChangeEvent, SyntheticEvent, useState } from "react";
-import "./App.css";
-import CardList from "./components/cardlist/CardList";
+import CardList from "./components/card-list/CardList";
 import Search from "./components/search/Search";
 import { CompanySearch } from "./company";
 import { searchCompanies } from "./api";
+import PortfolioList from "./components/portfolio/portfolio-list/PortfolioList";
+import Navbar from "./components/nav-bar/Navbar";
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
+  const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
-    console.log(e);
   };
 
-  const onClick = async (e: SyntheticEvent) => {
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+  };
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
     const result = await searchCompanies(search);
-    if (typeof result === "string"){
+    if (typeof result === "string") {
       setServerError(result);
     } else if (Array.isArray(result?.data)) {
       setSearchResult(result.data);
     }
-    console.log(searchResult);
   };
 
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange} />
-      <CardList />
+      <Navbar />
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      <PortfolioList
+        portfolioValues={portfolioValues}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
+      {serverError && <div>Unable to connect to API</div>}
     </div>
   );
 }
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,31 +1,35 @@
-import viteLogo from "/vite.svg";
-import "./Card.css";
-import { FC } from "react";
+import { FC, SyntheticEvent } from "react";
+import { CompanySearch } from "../../company";
+import AddPortfolio from "../portfolio/add-portfolio/AddPortfolio";
 
 interface Props {
-  companyName: string;
-  ticker: string;
-  price: number;
+  id: string;
+  searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
 const Card: FC<Props> = ({
-  companyName,
-  ticker,
-  price,
+  id,
+  searchResult,
+  onPortfolioCreate,
 }: Props): JSX.Element => {
   return (
-    <div className="card">
-      <img src={viteLogo} alt="Image" />
-      <div className="details">
-        <h2>
-          {companyName} ({ticker})
-        </h2>
-        <p>${price}</p>
-      </div>
-      <p className="info">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-        consequatur.
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   );
 };
